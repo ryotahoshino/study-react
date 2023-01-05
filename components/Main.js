69a1af2323925a7c3e44b0e648720a6929318ef6
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2023-01-05 20:10:28 +0900

    💄 スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import styles from './Main.module.css'
 import {Headline} from '../components/Headline'
 import { Links } from '../components/Links'
 
